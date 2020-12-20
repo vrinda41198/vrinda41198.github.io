0bb85285c2f4716fd76705d11adf69dcc37cbcdb
ApoorvAga <agarwalapoorv.1999@gmail.com>
2020-12-20 20:28:38 +0530

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,18 @@ $(function() {
 	        transition_delay: 500
 	    });
     }
+
+    const [red, green, blue] = [255, 255, 255]
+    const navlinks = document.getElementsByClassName('navbar-links')
+
+    window.addEventListener('scroll', () => {
+        let y = 1 + (window.scrollY || window.pageYOffset) / 150
+        y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
+        const [r, g, b] = [red / y, green / y, blue / y].map(Math.round)
+        for (let i = 0; i < navlinks.length; i++) {
+            navlinks[i].style.color = `rgb(${r}, ${g}, ${b})`
+        }
+    })
     
     $(document).bind('scroll', function(ev) {
         var scrollOffset = $(document).scrollTop();
